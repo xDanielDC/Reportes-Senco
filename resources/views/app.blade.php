35b5500adc0668082b43e8f9248f905edcf7ee7d
Daniel Chitiva <141698740+xDanielDC@users.noreply.github.com>
2026-02-05 15:32:49 -0500

    Manifest e iconos para accesos directos

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,10 @@
 
         <!-- Favicon -->
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#EF4444">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
